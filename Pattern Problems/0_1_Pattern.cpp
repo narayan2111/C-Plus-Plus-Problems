5713daa39d933f28831ee11cb30ea150490464b4
narayan2111 <narayandwivedi076@gmail.com>
2020-11-03 12:54:54 +0530

    0-1 Pattern

---
+++ Pattern Problems/0_1_Pattern.cpp
@@ -0,0 +1,40 @@
+// C++ Program to print 0 1 Pattern
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	for(int i=1;i<=n; i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			if((i+j)%2==0)
+			{
+				cout<<"1 ";
+			}
+			else
+			{
+				cout<<"0 ";
+			}
+		}cout<<endl;
+	}return 0 ;
+}
+
+/*
+Sample I/O:
+Input:
+5
+Output:
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+
+Time Complexity:O(n*n);
+Space Complexity:O(1)
+*/
