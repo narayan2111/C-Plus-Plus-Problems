cda1e553a8f5b840f149ef266c615f13fe8c0523
narayan2111 <narayandwivedi076@gmail.com>
2020-11-03 13:01:27 +0530

    Number Half Pyramid

---
+++ Pattern Problems/Number_Half_Pyramid.cpp
@@ -0,0 +1,36 @@
+// C++ Program to Print Half Pyramid Number
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i; j++)
+		{
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+/*
+Sample I/O:
+Input:
+5
+Output:
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+Time Complexity:O(n*n)
+Space Complexity:O(1)
+
+*/
