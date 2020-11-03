cc32b72eefbd593c8d7215d2e901b7e37a5ecd09
narayan2111 <narayandwivedi076@gmail.com>
2020-11-03 12:57:56 +0530

    Floyd's Triangle

---
+++ Pattern Problems/Floyd's_triangle.cpp
@@ -0,0 +1,38 @@
+// C++ Program to Print Floyd's Triangle
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int count =1;
+	cin>>n;
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i; j++)
+		{
+			cout<<count<<" ";
+			count++;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+/*
+Sample I/O:
+Input:
+5
+Output:
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+Time Complexity:O(n*n)
+Space Complexity:O(1)
+
+*/
